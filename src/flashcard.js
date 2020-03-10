0762bdf13f0686826d00e8a65ba1c55065147038
Charles Buxton <chazbux@gmail.com>
2020-03-10 16:26:30 -0600

    styling

--- src/flashcard.js
+++ src/flashcard.js
@@ -1,28 +1,37 @@
 import React from 'react'
-import { Button, Card, Checkbox } from 'semantic-ui-react'
+import { Button, Card, Checkbox, Icon } from 'semantic-ui-react'
 
 const FlashCards = (props) => (
   <Card.Group>
-    {props.cards.map( card => (
-    <Card key={`card-${card.id}`}>
-         <Card.Content extra>
-        <div className='ui two buttons'>
-        <Checkbox toggle label='Flip Card'/>
-        </div>
-      </Card.Content>
-      <Card.Content>
-        <Card.Description>
-          {card.front}
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className='ui two buttons'>
-          {/* <Checkbox label='I know this one!'/> */}
-        </div>
-      </Card.Content>
-    </Card>
+    {props.cards.map(card => (
+      <Card key={`card-${card.id}`}>
+        <Card.Content extra>
+          <div className='ui two buttons'>
+            <Checkbox toggle label='Flip Card' />
+
+          </div>
+        </Card.Content>
+        <Card.Content>
+          <Card.Description>
+            <div className="cardText">
+              {card.front}
+            </div>
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <div className='ui two buttons'>
+            {/* <Checkbox label='I know this one!'/> */}
+
+          </div>
+          <div className='iconPadding'>
+            <Icon bordered name='trash alternate' />
+          </div>
+        </Card.Content>
+
+      </Card>
     ))}
   </Card.Group>
+
 )
 
 export default FlashCards
